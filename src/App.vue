4fab61d6a66194383496e3dad527f0f01b0afbd9
Avi Sen <senavi@berkeley.edu>
2018-07-28 00:54:11 -0700

    added flask app fololder from old project

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div id="app">
     <img src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <CardBox/>
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
+import VueSocketio from 'vue-socket.io-extended'
 import App from './App.vue'
+import store from './store'
 
 Vue.config.productionTip = false
+// Vue.use(VueSocketio, `//${window.location.host}`, store)
 
 new Vue({
+  store,
   render: h => h(App)
 }).$mount('#app')
