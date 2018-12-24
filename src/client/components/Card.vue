51dc0a732e988552d5739ff6ff0ad9bd80b414f0
avi <senavi@berkeley.edu>
2018-12-24 02:38:57 -0800

    cards now emit to parent props, etc.

--- src/client/components/Card.vue
+++ src/client/components/Card.vue
@@ -1,10 +1,10 @@
 <template>
   <v-btn class='card'
     color='grey lighten-3'
-    @click='card_click'
-    :style='[style_object, selected_style]'
+    @click='$emit("card_click", card)'
+    :style='{color, top}'
   >
-    {{ value }}<br>{{ suit }}
+    {{ this.rank }}<br>{{ this.suit }}
   </v-btn>
 </template>
 
@@ -12,37 +12,34 @@
 // TODO: make cards a basic shape object (?) and then use it
 //       as a button or not as needed.
 
-import io from 'socket.io-client'
-
-const values = ['3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A', '2']
+const ranks = ['3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A', '2']
 const suits = ['♣', '♦', '♥', '♠']
 
 export default {
+
   name: 'Card',
+
   props: {
     card: Number,
     is_selected: Boolean
   },
-  data () {
-    return {
-      value: values[~~((this.card - 1) / 4)],
-      suit: suits[(this.card - 1) % 4],
-      style_object: {
-        color: [1, 2].includes((this.card - 1) % 4) ? '#ff0000' : '#000000'
-      }
-    }
-  },
+
   computed: {
-    selected_style () {
-      return {
-        top: this.is_selected ? '-10px' : '0px'
-      }
-    }
-  },
-  methods: {
-    card_click () {
-      this.$emit('card_click', this.card)
-      // this.socket.emit('card_click', {'card': this.card})
+
+    rank () {
+      return ranks[~~((this.card - 1) / 4)]
+    },
+
+    suit () {
+      return suits[(this.card - 1) % 4]
+    },
+
+    color () {
+      return [1, 2].includes((this.card - 1) % 4) ? '#ff0000' : '#000000'
+    },
+
+    top () {
+      return this.is_selected ? '-10px' : '0px'
     }
   }
 }
@@ -53,14 +50,11 @@ export default {
 .card {
   height:60px;
   width: 30px;
-  /* padding-left: 10px;
-  padding-right: 10px; */
   margin: 4px;
   font-size: 20px;
   text-align: center;
   text-decoration: none;
   display: inline-flex;
-  /* color: rgb(153, 153, 153); */
   border: none;
   border-radius: 4px;
 }
--- src/client/components/CardBox.vue
+++ src/client/components/CardBox.vue
@@ -1,20 +1,33 @@
 <template>
   <div>
     <br>
-      <span v-if='current_hand_str in ["", ": empty hand"]'>Click on cards to add them to your current hand!</span>
-      <span v-else>{{ current_hand_str }}</span>
-    <br><br>
+      <span 
+        v-if='current_hand_str.length === 0 || current_hand_str === ": empty hand"'
+      >
+        Click on cards to add them to your current hand!
+      </span>
+
+      <span
+        v-else
+      >
+        {{ current_hand_str }}
+      </span>
+
+    <br>
+    <br>
+
     <Card
       v-for='card in cards'
       :key='card'
       :card='card'
       :is_selected='cards_selected[card]'
-      @card_click='card_click'
+      @card_click='$emit("card_click", $event)'
     ></Card>
   </div>
 </template>
 
 <script>
+// @card_click='$emit("card_click", $event)'
 import io from 'socket.io-client'
 import Card from './Card.vue'
 
@@ -42,12 +55,6 @@ export default {
     current_hand_str () {
       return namespaced_getter(this.namespace, 'current_hand_str')
     },
-  },
-
-  methods: {
-    card_click (card) {
-      this.$emit('card_click', card)
-    }
   }
 }
 </script>
